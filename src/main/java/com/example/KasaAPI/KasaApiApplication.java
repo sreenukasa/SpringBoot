d6ac2dfe20c68772dd2a466a8ce430b2e2f953e3
sreenukasa <sreenukasa@gmail.com>
2019-07-11 13:41:59 +0530

    Add files via upload

---
+++ src/main/java/com/example/KasaAPI/KasaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.KasaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KasaAPI/KasaApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.KasaAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KasaApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
